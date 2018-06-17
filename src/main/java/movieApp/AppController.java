1cab3dc0b3f16f72ead32e98810eef336cd3ee7a
chudy1997 <karolbartyzel@interia.pl>
2018-06-17 15:13:09 +0200

    Set status codes of responses.

--- src/main/java/movieApp/AppController.java
+++ src/main/java/movieApp/AppController.java
@@ -2,15 +2,19 @@ package movieApp;
 
 import movieApp.model.movie.Movie;
 import movieApp.model.movie.MovieJSON;
-import movieApp.utils.MovieUtil;
 import movieApp.model.movie.StatusMovieJSON;
 import movieApp.model.user.User;
+import movieApp.utils.MovieUtil;
 import movieApp.utils.UserUtil;
-import org.springframework.http.*;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.*;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 @Controller
 public class AppController {
@@ -21,73 +25,88 @@ public class AppController {
     @CrossOrigin(origins = ORIGINS)
     @PostMapping("/login")
     @ResponseBody
-    public String loginUser(String userId) {
+    public String loginUser(String userId, HttpServletResponse response) {
         final String uniqueToken = UserUtil.getUniqueToken();
         users.put(uniqueToken, new User(userId));
+
+        response.setStatus(HttpStatus.OK.value());
         return uniqueToken;
     }
 
     @CrossOrigin(origins = ORIGINS)
     @PostMapping("/logout")
     @ResponseBody
-    public Object logoutUser(String token) {
+    public Object logoutUser(String token, HttpServletResponse response) {
         users.remove(token);
-        return HttpStatus.OK;
+
+        response.setStatus(HttpStatus.OK.value());
+        return "";
     }
 
     @CrossOrigin(origins = ORIGINS)
     @GetMapping("/movies")
     @ResponseBody
-    public List<StatusMovieJSON> getMovies(String token) {
+    public List<StatusMovieJSON> getMovies(String token, HttpServletResponse response) {
         final User user = UserUtil.getUser(users, token);
+
+        response.setStatus(HttpStatus.OK.value());
         return MovieUtil.getMoviesInfo(user.getMovies());
     }
 
     @CrossOrigin(origins = ORIGINS)
     @GetMapping("/movie")
     @ResponseBody
-    public MovieJSON getMovieByTitle(String token, String title) {
+    public MovieJSON getMovieByTitle(String token, String title, HttpServletResponse response) {
         final User user = UserUtil.getUser(users, token);
+
+        response.setStatus(HttpStatus.OK.value());
         return MovieUtil.getMovieInfo(Objects.requireNonNull(MovieUtil.getMovieFromTitle(user, title)));
     }
 
     @CrossOrigin(origins = ORIGINS)
     @PostMapping("/movie/status")
     @ResponseBody
-    public Object setMovieStatus(String token, String title, Boolean status) {
+    public Object setMovieStatus(String token, String title, Boolean status, HttpServletResponse response) {
         final User user = UserUtil.getUser(users, token);
         UserUtil.setMovieStatus(user, title, status);
-        return HttpStatus.OK;
+
+        response.setStatus(HttpStatus.OK.value());
+        return "";
     }
 
     @CrossOrigin(origins = ORIGINS)
     @PostMapping("/movies/new")
     @ResponseBody
-    public Object addMovie(String token, String title) {
+    public Object addMovie(String token, String title, HttpServletResponse response) {
         final User user = UserUtil.getUser(users, token);
         final String parsedTitle = title.trim().toLowerCase();
         final Movie newMovie = new Movie(parsedTitle);
 
-        if(MovieUtil.getMovieFromTitle(user, parsedTitle) != null) {
-            return HttpStatus.ALREADY_REPORTED;
-        }
-
         MovieJSON movieJSON = MovieUtil.getMovieInfo(newMovie);
         if(movieJSON == null || movieJSON.getId().equals("")) {
-            return HttpStatus.BAD_REQUEST;
+            response.setStatus(HttpStatus.BAD_REQUEST.value());
+            return "Movie with such title does not exist.";
+        }
+
+        if(MovieUtil.getMovieFromTitle(user, movieJSON.getTitle()) != null) {
+            response.setStatus(HttpStatus.ALREADY_REPORTED.value());
+            return "Movie with such title is already added.";
         }
 
         user.addMovie(movieJSON.getTitle());
-        return HttpStatus.CREATED;
+        response.setStatus(HttpStatus.CREATED.value());
+        return "";
     }
 
     @CrossOrigin(origins = ORIGINS)
     @DeleteMapping("/movies/delete")
     @ResponseBody
-    public Object deleteMovie(String token, String title) {
+    public Object deleteMovie(String token, String title, HttpServletResponse response) {
         final User user = UserUtil.getUser(users, token);
         user.deleteMovie(MovieUtil.getMovieFromTitle(user, title));
-        return HttpStatus.OK;
+
+        response.setStatus(HttpStatus.OK.value());
+        return "";
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////
